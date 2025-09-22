5e2fa4f36dbf8794e6f44449744ba66a2c9f90ce
RaghavOjha <jeh267@txstate.edu>
2025-09-22 18:26:57 -0500

    added production config

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // production optimizations
+  compress: true,
+  poweredByHeader: false,
+  generateEtags: false,
 }
 
 export default nextConfig
